58a3be9e558baf5e626dc9cc4790c16c33015f70
10.210.0.67 <10.210.0.67@example.com>
2021-03-17 14:00:17 +0800

    Initial lab1

--- ray/sh_test2/search.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-#First you can use grep (-n) to find the number of lines of string.
-#grep -n "$2" $1 | cut -f1 -d: > $3
-grep -n "$2" $1 | awk -F: '{print $1}' > $3
-#Then you can use awk to separate the answer.
-
