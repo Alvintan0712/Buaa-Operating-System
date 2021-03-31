79a385ab4dc4d1008ea876e4b3905a257a68f683
陈劲安 <79066012@buaa.edu.cn>
2021-03-31 20:17:33 +0800

    submit lab1

--- lib/printf.c
+++ lib/printf.c
@@ -24,11 +24,11 @@ static void myoutput(void *arg, char *s, int l)
     int i;
 
     // special termination call
-    if ((l==1) && (s[0] == '\0')) return;
-    
-    for (i=0; i< l; i++) {
-	printcharc(s[i]);
-	if (s[i] == '\n') printcharc('\n');
+    if ((l == 1) && (s[0] == '\0')) return;
+
+    for (i = 0; i < l; i++) {
+		printcharc(s[i]);
+		if (s[i] == '\n') printcharc('\n');
     }
 }
 
@@ -40,18 +40,15 @@ void printf(char *fmt, ...)
     va_end(ap);
 }
 
-void
-_panic(const char *file, int line, const char *fmt,...)
+void _panic(const char *file, int line, const char *fmt,...)
 {
 	va_list ap;
-
 
 	va_start(ap, fmt);
 	printf("panic at %s:%d: ", file, line);
 	lp_Print(myoutput, 0, (char *)fmt, ap);
 	printf("\n");
 	va_end(ap);
-
 
 	for(;;);
 }
