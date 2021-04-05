7c7ff38bd84fbed299795f3cfd0c0f7cad24793c
陈劲安 <79066012@buaa.edu.cn>
2021-04-05 11:26:28 +0800

    just for code style

--- include/mmu.h
+++ include/mmu.h
@@ -32,12 +32,12 @@
 /* Page Table/Directory Entry flags
  *   these are defined by the hardware
  */
-#define PTE_G		0x0100	// Global bit
-#define PTE_V		0x0200	// Valid bit
-#define PTE_R		0x0400	// Dirty bit ,'0' means only read ,otherwise make interrupt
-#define PTE_D		0x0002	// fileSystem Cached is dirty
-#define PTE_COW		0x0001	// Copy On Write
-#define PTE_UC		0x0800	// unCached
+#define PTE_G			0x0100	// Global bit
+#define PTE_V			0x0200	// Valid bit
+#define PTE_R			0x0400	// Dirty bit ,'0' means only read ,otherwise make interrupt
+#define PTE_D			0x0002	// fileSystem Cached is dirty
+#define PTE_COW			0x0001	// Copy On Write
+#define PTE_UC			0x0800	// unCached
 #define PTE_LIBRARY		0x0004	// share memmory
 /*
  * Part 2.  Our conventions.
@@ -108,19 +108,19 @@
 #define E_UNSPECIFIED	1	// Unspecified or unknown problem
 #define E_BAD_ENV       2       // Environment doesn't exist or otherwise
 // cannot be used in requested action
-#define E_INVAL		3	// Invalid parameter
-#define E_NO_MEM	4	// Request failed due to memory shortage
+#define E_INVAL			3	// Invalid parameter
+#define E_NO_MEM		4	// Request failed due to memory shortage
 #define E_NO_FREE_ENV   5       // Attempt to create a new environment beyond
 // the maximum allowed
 #define E_IPC_NOT_RECV  6	// Attempt to send to env that is not recving.
 
 // File system error codes -- only seen in user-level
-#define	E_NO_DISK	7	// No free space left on disk
-#define E_MAX_OPEN	8	// Too many files are open
-#define E_NOT_FOUND	9 	// File or block not found
-#define E_BAD_PATH	10	// Bad path
+#define	E_NO_DISK		7	// No free space left on disk
+#define E_MAX_OPEN		8	// Too many files are open
+#define E_NOT_FOUND		9 	// File or block not found
+#define E_BAD_PATH		10	// Bad path
 #define E_FILE_EXISTS	11	// File already exists
-#define E_NOT_EXEC	12	// File not a valid executable
+#define E_NOT_EXEC		12	// File not a valid executable
 
 #define MAXERROR 12
 
@@ -173,5 +173,5 @@ extern volatile Pde *vpd[];
 
 extern void tlb_out(u_int entryhi);
 
-#endif //!__ASSEMBLER__
+#endif // !__ASSEMBLER__
 #endif // !_MMU_H_
