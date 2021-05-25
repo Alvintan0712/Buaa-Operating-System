947024332dd78a637df158a4f03e03cf2da1a898
陈劲安 <79066012@buaa.edu.cn>
2021-05-25 18:15:10 +0800

    exec 6.2

--- fs/ide.c
+++ fs/ide.c
@@ -27,10 +27,24 @@ void ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 	int offset_begin = secno * 0x200;
 	int offset_end = offset_begin + nsecs * 0x200;
 	int offset = 0;
+	const u_int base = 0x13000000;
 
 	while (offset_begin + offset < offset_end) {
         // Your code here
         // error occurred, then panic.
+		u_int tmp = offset_begin + offset;
+		u_int dev_id = 0;
+		u_int enable = 0;
+		u_int status;
+
+		if (syscall_write_dev(&tmp, base, 4))) user_panic("read IDE disk error");
+		if (syscall_write_dev(&dev_id, base + 0x0010, 4)) user_panic("read IDE disk error");
+		if (syscall_write_dev(&enable, base + 0x0020, 4)) user_panic("read IDE disk error");
+		if (syscall_read_dev(&status, base + 0x0030, 4)) user_panic("read IDE disk error");
+		if (status == 0) user_panic("read IDE disk error");
+		if (syscall_read_dev(dst + offset, base + 0x4000, 0x200)) user_panic("read IDE disk error");
+
+		offset += 0x200;
 	}
 }
 
@@ -51,17 +65,29 @@ void ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 void ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
 {
     // Your code here
-	// int offset_begin = ;
-	// int offset_end = ;
-	// int offset = ;
+	int offset_begin = secno * 0x200;
+	int offset_end = offset_begin + nsecs * 0x200;
+	int offset = 0;
+	const u_int base = 0x13000000;
 
 	// DO NOT DELETE WRITEF !!!
 	writef("diskno: %d\n", diskno);
 
-	// while ( < ) {
+	while (offset_begin + offset < offset_end) {
 	    // copy data from source array to disk buffer.
-
 		// if error occur, then panic.
-	// }
-}
+		u_int tmp = offset_begin + offset;
+		u_int dev_id = 0;
+		u_int enable = 1;
+		u_int status;
 
+		if (syscall_write_dev(src + offset, base + 0x4000, 0x200)) user_panic("write IDE disk error");
+		if (syscall_write_dev(&tmp, base, 4))) user_panic("write IDE disk error");
+		if (syscall_write_dev(&dev_id, base + 0x0010, 4)) user_panic("write IDE disk error");
+		if (syscall_write_dev(&enable, base + 0x0020, 4)) user_panic("write IDE disk error");
+		if (syscall_read_dev(&status, base + 0x0030, 4)) user_panic("write IDE disk error");
+		if (status == 0) user_panic("write IDE disk error");
+
+		offset += 0x200;
+	}
+}
