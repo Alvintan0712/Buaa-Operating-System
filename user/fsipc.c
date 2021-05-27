1d395ba00b3537cf53ed7625d19dfc8f636edd5d
陈劲安 <79066012@buaa.edu.cn>
2021-05-27 12:16:11 +0800

    apa lancau

--- user/fsipc.c
+++ user/fsipc.c
@@ -121,7 +121,7 @@ int fsipc_dirty(u_int fileid, u_int offset)
 int fsipc_remove(const char *path)
 {
 	// Step 1: Check the length of path, decide if the path is valid.
-	if (strlen(path) >= MAXPATHLEN) return -E_BAD_PATH;
+	if (path[0] == '\0' || strlen(path) >= MAXPATHLEN) return -E_BAD_PATH;
 	// Step 2: Transform fsipcbuf to struct Fsreq_remove*
 	struct Fsreq_remove *rq = (struct Fsreq_remove *) fsipcbuf;
 	// Step 3: Copy path to path in req.
