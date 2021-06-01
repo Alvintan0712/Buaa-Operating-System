a272992a4767c4390430d8bd6715d9154de1970d
10.210.0.67 <10.210.0.67@example.com>
2021-06-01 18:11:37 +0800

    Initial lab6

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,7 +3,8 @@
 
 #include "lib.h"
 
-void umain(void)
+void
+umain(void)
 {
 	u_int who, i;
 
@@ -15,19 +16,20 @@ void umain(void)
 	}
 
 	for (;;) {
-		writef("%x am waiting.....\n", syscall_getenvid());
+		writef("%x am waiting.....\n",syscall_getenvid());
 		i = ipc_recv(&who, 0, 0);
-
+		
 		writef("%x got %d from %x\n", syscall_getenvid(), i, who);
-
+	
 		//user_panic("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-		if (i == 10) return;
-
+		if (i == 10)
+			return;
 		i++;
-		writef("\n@@@@@send %d from %x to %x\n",i, syscall_getenvid(), who);
+		writef("\n@@@@@send 0 from %x to %x\n", syscall_getenvid(), who);
 		ipc_send(who, i, 0, 0);
-
-		if (i == 10) return;
+		if (i == 10)
+			return;
 	}
-
+		
 }
+
